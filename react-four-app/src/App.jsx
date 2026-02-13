1da09677599041914f6a665cfd5879843454b829
Adityaiwt <aadityakumar4984@gmail.com>
2026-02-13 17:25:20 +0530

    Build a TicketNum and Ticket

--- react-four-app/src/App.jsx
+++ react-four-app/src/App.jsx
@@ -2,15 +2,16 @@ import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
+import Ticket from './Ticket'
 
 
 function App() {
 
   return (
     <>
-      <Lottery />
+      <Ticket ticket={[0, 1, 2]} />
+      <Ticket ticket={[3, 4, 5, 6]} />
     </>
-  )
+  );
 }
-
 export default App
