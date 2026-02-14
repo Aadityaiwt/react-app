a4dfb0556c7edf8ba067a0e6367ce3c5b0fdc025
Adityaiwt <aadityakumar4984@gmail.com>
2026-02-14 20:17:44 +0530

    Function as Props

--- react-four-app/src/App.jsx
+++ react-four-app/src/App.jsx
@@ -2,15 +2,18 @@ import './App.css'
 import LudoBoard from './LudoBoard'
 import TodoList from './TodoList'
 import Lottery from './Lottery'
-import Ticket from './Ticket'
+import { sum } from './helper'
 
 
 function App() {
 
+  let winCondition = (ticket) => {
+    return ticket[0] === 0;
+  };
+
   return (
     <>
-      <Ticket ticket={[0, 1, 2]} />
-      <Ticket ticket={[3, 4, 5, 6]} />
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   );
 }
